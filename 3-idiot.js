105535f69a797c6d98e1261757d9c01b88bc5c06
Bappy <bappymahmud321@gmail.com>
2023-02-20 15:46:04 +0600

    added content

---
+++ 3-idiot.js
@@ -0,0 +1,36 @@
+// const max = Math.max(12, 24, 353, 34) ;
+// // console.log(max) ;
+// const numbers = [12, 24, 353, 34] ;
+// const largest = Math.max(...numbers);
+// console.log(...numbers) ;
+// console.log(largest) ;
+
+// const numbers2 = numbers ;
+
+
+
+// const fish = {
+//     name: 'king hilsha ',
+//     adress: 'chandpur',
+//     color: 'silver',
+//     phone: '01289187923',
+//     price: 4000
+// }
+// const {name, adress, color, phone, price} = { name: 'king hilsha ',
+// adress: 'chandpur',
+// color: 'silver',
+// phone: '01289187923',
+// price: 4000}
+// console.log(name) ;
+
+
+// const phone = fish.phone ;
+// const color = fish.color ;
+
+// const {phone}= fish 
+// console.log(phone) ;
+// console.log(color) ;
+
+// array distructuring
+const [first, another] = [44,99]
+console.log(first,another)
--- template.js
+++ template.js
@@ -1,6 +1,17 @@
-function add(first, second){
-    const total = first + second ;
-    return total;
-}
-const result = add(10,20)
-console.log(result) ;
+// function add(first, second){
+//     const total = first + second ;
+//     return total;
+// }
+
+// const number = 56
+// const add1 = function add1(first, second){
+//     const total = first + second;
+//     return total ;
+// }
+// const result = add(10,20)
+// console.log(result) ;
+
+const add = (first, second) => first + second ;
+const getFullName = (first, last) => first + ' ' + last ;
+const multiply = (a, b) => a * b ;
+const result = multiply (7, 8) ;
